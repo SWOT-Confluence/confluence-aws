14e3f0f0355e6c662cda9ba90053c8e12322171b
Nikki Tebaldi <ntebaldi@umass.edu>
2021-06-21 11:39:29 -0400

    Algorithm class and tests
    - Responsible for creating and submitting jobs needed for full
    execution of the algorithm.

---
+++ confluence/Algorithm.py
@@ -0,0 +1,97 @@
+# Third-party imports
+import botocore
+
+# Local imports
+from confluence.Job import Job
+
+class Algorithm:
+    """
+    A class that represents an algorithm requiring execution in Confluence.
+
+    An Algorithm usually needs to be submitted to AWS Batch as a set of jobs. An
+    Algorithm creates Job objects to handle the submission operation.
+
+    Attributes
+    ----------
+    array_size: int
+        size of the AWS Batch job array for each job
+    input_files: list
+        list of input files that are submitted as arguments to a job
+    job_ids: list
+        list of job identifiers for jobs submitted to AWS Batch
+    jobs: list
+        list of Job objects that handle submission to AWS Batch
+    name: str
+        name of the algorithm
+    num_jobs: int
+        number of jobs to be created
+    
+    Methods
+    -------
+    create_jobs(stage, dependencies)
+        creates jobs that can be submitted to AWS Batch
+    submit_jobs()
+        submits jobs to AWS Batch
+    """
+
+    def __init__(self, name, num_jobs, array_size, input_files):
+        """
+        Parameters
+        ----------
+        name: str
+            name of algorithm
+        num_jobs: int
+            the number of jobs to be created
+        array_size: int
+            the size of the AWS Batch job array for each job
+        input_files: list
+            list of input files that are submitted as arguments for each job
+        """
+
+        self.array_size = array_size
+        self.input_files = input_files
+        self.job_ids = []
+        self.jobs = []
+        self.name = name
+        self.num_jobs = num_jobs
+
+    def create_jobs(self, stage):
+        """Create Job objects that are responsible for running the algorithm
+        in AWS Batch.
+
+        Parameters
+        ----------
+            stage: str
+                name of the stage that the algorithm is a part of
+        """
+
+        for i in range(self.num_jobs):
+            job = Job(name=f"{stage}_{self.name}_{i}", job_def=self.name,
+                queue=stage)
+            if (len(self.input_files) > 0): job.define_arguments([self.input_files[i]])
+            job.define_array(self.array_size)
+            job.define_tags(tag_dict={ "job": f"{stage}_{self.name}_{i}" },
+                will_propagate=True)
+            self.jobs.append(job)
+
+    def submit_jobs(self, dependencies):
+        """Submits jobs to AWS Batch job queue.
+
+        Parameters
+        ----------
+        dependencies: list
+            list of job identifiers that the Algorithm's jobs depend on
+        
+        Raises
+        ------
+        botocore.exceptions.ClientError
+            if AWS Batch API returns an error response upon job submission
+        """
+
+        for job in self.jobs:
+            try:
+                job.define_dependencies(dependencies)
+                job_id = job.submit()
+                self.job_ids.append(job_id)
+            except botocore.exceptions.ClientError as error:
+                raise error
---
+++ tests/test_Algorithm.py
@@ -0,0 +1,71 @@
+# Standard imports
+import unittest
+from unittest.mock import patch
+
+# Local imports
+from confluence.Algorithm import Algorithm
+
+class TestAlgorithm(unittest.TestCase):
+    """Tests methods from Algorithm class."""
+
+    DEPENDENCIES = ["f00afeb4-354a-4e80-a226-0021a8b38e32",
+                    "397fdcd6-5af3-4003-862f-03fcb6594cce",
+                    "81bf7409-2ea3-4baa-93b8-3c2613b172d0",
+                    "fa1f2f66-8975-44ea-87f0-ff569e1302d8",
+                    "e4bd42df-7330-4de3-9ea2-a8880c57c488"]
+    EXPECTED_DEPS = [
+            { "jobId": "f00afeb4-354a-4e80-a226-0021a8b38e32" },
+            { "jobId": "397fdcd6-5af3-4003-862f-03fcb6594cce" },
+            { "jobId": "81bf7409-2ea3-4baa-93b8-3c2613b172d0" },
+            { "jobId": "fa1f2f66-8975-44ea-87f0-ff569e1302d8" },
+            { "jobId": "e4bd42df-7330-4de3-9ea2-a8880c57c488" }
+        ]
+    INPUT_FILES = ["reaches_1.txt", "reaches_2.txt", "reaches_3.txt"]
+
+    def test_create_jobs(self):
+        """Tests the create_jobs method."""
+
+        alg = Algorithm("test_alg", 3, 500, self.INPUT_FILES)
+        alg.create_jobs("test_flpe")
+        self.assertEqual(3, len(alg.jobs))
+
+        job = alg.jobs[1]
+        self.assertEqual({ "size": 500 }, job.array_props)
+        self.assertEqual({ "command": ["reaches_2.txt"] }, job.overrides)
+        self.assertEqual("test_alg", job.job_def)
+        self.assertEqual("test_flpe_test_alg_1", job.name)
+        self.assertTrue(job.propagate_tags)
+        self.assertEqual("test_flpe", job.queue)
+        self.assertEqual({ "attempts": 1 }, job.retry_strategy)
+        self.assertEqual({ "job": "test_flpe_test_alg_1"}, job.tags)
+
+    @patch("confluence.Job.boto3", autospec=True)
+    def test_submit_jobs(self, mock_boto):
+        """Test submit_jobs method."""
+
+        mock_boto.client("batch").submit_job.side_effect = [
+            { "jobArn": "amazon-resource-name", 
+              "jobName": "test_flpe_test_alg_0", 
+              "jobId": "d90d061b-c16d-4a47-ba25-260727bac56b" }, 
+            { "jobArn": "amazon-resource-name", 
+              "jobName": "test_flpe_test_alg_1", 
+              "jobId": "134ee8b1-0127-4c4e-aa6d-9bfbcd581aa6" },
+            { "jobArn": "amazon-resource-name", 
+              "jobName": "test_flpe_test_alg_1", 
+              "jobId": "1d4b37c6-7dfb-4301-99f0-6181fe3ba124" }
+        ]
+
+        alg = Algorithm("test_alg", 3, 500, self.INPUT_FILES)
+        alg.create_jobs("test_flpe")
+        alg.submit_jobs(self.DEPENDENCIES)
+
+        self.assertEqual(self.EXPECTED_DEPS, alg.jobs[0].depends_on)
+        self.assertEqual(self.EXPECTED_DEPS, alg.jobs[1].depends_on)
+        self.assertEqual(self.EXPECTED_DEPS, alg.jobs[2].depends_on)
+
+        expected = ["d90d061b-c16d-4a47-ba25-260727bac56b", 
+                    "134ee8b1-0127-4c4e-aa6d-9bfbcd581aa6", 
+                    "1d4b37c6-7dfb-4301-99f0-6181fe3ba124"]
+        self.assertEqual(expected, alg.job_ids)
+
+        
