1b8b7935ea36d3be065d4e943f0f80b12a89bbc0
Nikki Tebaldi <17799906+nikki-t@users.noreply.github.com>
2023-01-20 16:51:46 -0500

    Add option to run on simulated data hosted in Confluence S3

--- run_confluence.py
+++ run_confluence.py
@@ -2,6 +2,11 @@
 
 This script runs the Confluence workflow and submits jobs to AWS Batch.
 
+Arguements:
+  -c: Path to YAML configuration file
+  -s: Indicates simulated data run
+  -k: Unique SSM encryption key identifier
+
 PyYAML must be installed in the environment prior to execution.
 
 It requires the following classes: Confluence, Stage, Algorithm and Job all 
@@ -11,20 +16,41 @@ This script can also be imported as a module and contains the following
 functions:
     * create_logger - creates a logger object used to log status
     * main - the main entrypoint of the script
+    
+Example execution: python3 run_confluence.py -c /path/to/confluence.yaml
 """
 
 # Standard imports
+import argparse
 from datetime import datetime
 import logging
 from pathlib import Path
 
 # Third-party imports
+import boto3
+import botocore
 import yaml
 
 # Local imports
 from confluence.Confluence import Confluence
 
-CONFIG_FILE = Path("")
+def create_args():
+    """Create and return argparser with arguments."""
+
+    arg_parser = argparse.ArgumentParser(description="Retrieve a list of S3 URIs")
+    arg_parser.add_argument("-c",
+                            "--configyaml",
+                            type=str,
+                            help="Path to YAML configuration file")
+    arg_parser.add_argument("-s",
+                        "--simulated",
+                        help="Indication to run on simulated data",
+                        action="store_true")
+    arg_parser.add_argument("-k",
+                            "--ssmkey",
+                            type=str,
+                            help="Unique SSM encryption key identifier.")
+    return arg_parser
 
 def create_logger(log_to_console=True, log_file=None, log_to_file=False):
     """Creates and sets a Logger object to allow logging of status.
@@ -68,18 +94,87 @@ def create_logger(log_to_console=True, log_file=None, log_to_file=False):
 
     return logger
 
+def store_s3_creds(key):
+    """Get temporary creds for S3-hosted simulated data.
+    
+    Note data is stored in Confluence AWS and the credentials are determined
+    from what ever user account runs this script.
+    """
+    
+    # Retrieve temporary credentials
+    client = boto3.client('sts')
+    response = client.get_session_token()
+    creds = {
+        "accessKeyId": response["Credentials"]["AccessKeyId"],
+        "secretAccessKey": response["Credentials"]["SecretAccessKey"],
+        "sessionToken": response["Credentials"]["SessionToken"],
+        "expiration": response["Credentials"]["Expiration"].strftime("%Y-%m-%d %H:%M:%S+00:00")
+    }
+    
+    # Store temporary credentials in parameter store    
+    ssm_client = boto3.client('ssm', region_name="us-west-2")
+    try:
+        response = ssm_client.put_parameter(
+            Name="s3_creds_key",
+            Description="Temporary SWOT S3 bucket key",
+            Value=creds["accessKeyId"],
+            Type="SecureString",
+            KeyId=key,
+            Overwrite=True,
+            Tier="Standard"
+        )
+        response = ssm_client.put_parameter(
+            Name="s3_creds_secret",
+            Description="Temporary SWOT S3 bucket secret",
+            Value=creds["secretAccessKey"],
+            Type="SecureString",
+            KeyId=key,
+            Overwrite=True,
+            Tier="Standard"
+        )
+        response = ssm_client.put_parameter(
+            Name="s3_creds_token",
+            Description="Temporary SWOT S3 bucket token",
+            Value=creds["sessionToken"],
+            Type="SecureString",
+            KeyId=key,
+            Overwrite=True,
+            Tier="Standard"
+        )
+        response = ssm_client.put_parameter(
+            Name="s3_creds_expiration",
+            Description="Temporary SWOT S3 bucket expiration",
+            Value=creds["expiration"],
+            Type="SecureString",
+            KeyId=key,
+            Overwrite=True,
+            Tier="Standard"
+        )
+    except botocore.exceptions.ClientError:
+        raise
+
 def main():
     """Execute Confluence workflow."""
 
     start = datetime.now()
-    with open(CONFIG_FILE) as yaml_file:
+    # Get command line arguments and config data
+    arg_parser = create_args()
+    args = arg_parser.parse_args()
+    with open(args.configyaml) as yaml_file:
         config_data = yaml.safe_load(yaml_file)
 
+    # Get a logger to log to file
     log_file = Path(config_data["log_file"]) \
         if len(config_data["log_file"]) != 0 else None
     logger = create_logger(log_file=log_file, log_to_file=True)
+    
+    # Store temporary creds if simulated run
+    if args.simulated:
+        logger.info("Storing S3 credentials for run on simulated data.")
+        store_s3_creds(args.ssmkey)
 
-    confluence = Confluence(CONFIG_FILE)
+    # Submit AWS Batch jobs
+    confluence = Confluence(args.configyaml)
     confluence.create_stages()
     confluence.execute_stages(logger)
 
